9a966b85fc36f18c766dbd64e380a573b4197c27
Maria Rita <100208753+projetoMariaritado@users.noreply.github.com>
2023-05-16 09:51:13 -0300

    Add files via upload

---
+++ Cpf e Cnpj/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace exercicio02;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Contrato c = new Contrato();
+        ContratoPessoaFisica cf = new ContratoPessoaFisica();
+        ContratoPessoaJuridica cj = new ContratoPessoaJuridica();
+
+        cf.Tipo = "Pessoa física";
+        cf.Idade = 69;
+        cf.Cpf = 44149248800;
+        cj.Tipo = "Pessoa Jurídica";
+        cj.Cnpj = 42520353000162;
+        cj.Inscricao = 037689759148;
+
+        Console.WriteLine("Tipo de contrato: " + cf.Tipo + " -- Idade: " + cf.Idade + " -- CPF: " +cf.Cpf);
+        Console.WriteLine("Tipo de contrato: " + cj.Tipo + " -- CNPJ: " + cj.Cnpj + " -- Inscrição estadual: " +cj.Inscricao);
+    }
+} 
+
---
+++ Prestação/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace exercicio03;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Contrato c = new Contrato();
+        ContratoPessoaFisica cf = new ContratoPessoaFisica();
+        ContratoPessoaJuridica cj = new ContratoPessoaJuridica();
+
+        cf.Tipo = "Pessoa física";
+        cf.Idade = 69;
+        cf.Cpf = 44149248800;
+        cj.Tipo = "Pessoa Jurídica";
+        cj.Cnpj = 42520353000162;
+        cj.Inscricao = 037689759148;
+        cj.Prazo = 12;
+        cf.Prazo = 12;
+       
+
+        Console.WriteLine("Tipo de contrato: " + cf.Tipo + "| Idade: " + cf.Idade + "| CPF: " +cf.Cpf + "| Valor do contrato: " + c.CalcularPrestacao() + "| Prazo de pagamento: "+ cf.Prazo + "| Valor das prestações: " + cf.CalcularPrestacao());
+        Console.WriteLine("Tipo de contrato: " + cj.Tipo + "| CNPJ: " + cj.Cnpj + "| Inscrição estadual: " +cj.Inscricao + "| Valor do contrato: " + c.CalcularPrestacao() + "| Prazo de pagamento: " + cj.Prazo + "| Valor das prestações: " + cj.CalcularPrestacao());
+        
+    }
+} 
+
